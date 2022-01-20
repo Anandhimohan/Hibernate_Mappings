fc05f5fe825a838423e79b48b500876a8834c804
Anandhimohan <97152553+Anandhimohan@users.noreply.github.com>
2022-01-19 20:42:56 -0800

    Add files via upload
    One_One , One_many mapping

---
+++ src/main/java/com/cg/edubridge/Onetomany/Employeee.java
@@ -0,0 +1,50 @@
+package com.cg.edubridge.Onetomany;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity  
+@Table(name="emp340")  
+public class Employeee {
+	   
+	      
+	    @Id  
+	    @GeneratedValue(strategy=GenerationType.AUTO)  
+	    @PrimaryKeyJoinColumn  
+	private int employeeId;    
+	private String name,email;    
+	@OneToOne(targetEntity=EmpAdress.class,cascade=CascadeType.ALL)  
+	private EmpAdress address;  
+	public int getEmployeeId() {  
+	    return employeeId;  
+	}  
+	public void setEmployeeId(int employeeId) {  
+	    this.employeeId = employeeId;  
+	}  
+	public String getName() {  
+	    return name;  
+	}  
+	public void setName(String name) {  
+	    this.name = name;  
+	}  
+	public String getEmail() {  
+	    return email;  
+	}  
+	public void setEmail(String email) {  
+	    this.email = email;  
+	}  
+	public EmpAdress getAddress() {  
+	    return address;  
+	}  
+	public void setAddress(EmpAdress address) {  
+	    this.address = address;  
+	}    
+	  
+	}    
+
---
+++ src/main/java/com/cg/edubridge/Onetomany/StoreDetails.java
@@ -0,0 +1,44 @@
+package com.cg.edubridge.Onetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class StoreDetails {
+	
+	public static void main(String[] args) {    
+	      
+	    StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+	    Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();  
+	      
+	    SessionFactory factory=meta.getSessionFactoryBuilder().build();  
+	    Session session=factory.openSession();  
+	      
+	    Transaction t=session.beginTransaction();   
+	      
+	    Employeee e1=new Employeee();    
+	    e1.setName("Ravi Malik");    
+	    e1.setEmail("ravi@gmail.com");    
+	        
+	    EmpAdress address1=new EmpAdress();    
+	    address1.setAddressLine1("G-21,Lohia nagar");    
+	    address1.setCity("Ghaziabad");    
+	    address1.setState("UP");    
+	    address1.setCountry("India");    
+	    address1.setPincode(201301);    
+	        
+	    e1.setAddress(address1);    
+	    address1.setEmployee(e1);    
+	        
+	    session.persist(e1);    
+	    t.commit();    
+	        
+	    session.close();    
+	    System.out.println("success");    
+	}    
+	}    
+
